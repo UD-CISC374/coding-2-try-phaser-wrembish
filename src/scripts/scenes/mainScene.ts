2de9648557ee52819793b029a41904ba9baba11f
wrembish <wrembish@udel.edu>
2020-03-04 18:04:58 -0500

    more code cleanup

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -3,7 +3,6 @@ import { Beam } from '../objects/beam';
 import { Explosion } from '../objects/explosion';
 
 export default class MainScene extends Phaser.Scene {
-
   // tilesprites
   private background: Phaser.GameObjects.TileSprite;
 
@@ -45,7 +44,6 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-
     // add sprites
     this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, "background");
     this.background.setOrigin(0, 0);
@@ -56,71 +54,35 @@ export default class MainScene extends Phaser.Scene {
     this.ship5 = this.add.sprite(0, 0, "ship2");
     this.ship6 = this.add.sprite(0, 0, "ship3");
     this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "player");
-
-    // add the enemies to the group and activate physics for them
-    this.enemies = this.physics.add.group();
-    this.enemies.add(this.ship1);
-    this.enemies.add(this.ship2);
-    this.enemies.add(this.ship3);
-    this.enemies.add(this.ship4);
-    this.enemies.add(this.ship5);
-    this.enemies.add(this.ship6);
-
-    // make the enemies able to be interacted with
-    this.ship1.setInteractive();
-    this.ship2.setInteractive();
-    this.ship3.setInteractive();
-    this.ship4.setInteractive();
-    this.ship5.setInteractive();
-    this.ship6.setInteractive();
-
-    // animate the enemies
-    this.ship1.play("ship1_anim");
-    this.ship2.play("ship2_anim");
-    this.ship3.play("ship3_anim");
-    this.ship4.play("ship1_anim");
-    this.ship5.play("ship2_anim");
-    this.ship6.play("ship3_anim");
-
-
-    // create the group of power ups
-    this.powerUps = this.physics.add.group();
-
-    // fill the group with powerups
-    var maxObjects = 4;
-    for (var i = 0; i <= maxObjects; i++) {
-      var powerUp = this.physics.add.sprite(16, 16, "power-up");
-      this.powerUps.add(powerUp);
-      powerUp.setRandomPosition(0, 0, this.scale.width, this.scale.height);
-
-      if (Math.random() > 0.5) {
-        powerUp.play("red");
-      } else {
-        powerUp.play("gray");
-      }
-
-      powerUp.setVelocity(100, 100);
-      powerUp.setCollideWorldBounds(true);
-      powerUp.setBounce(1);
-    }
-
-    this.player.play("thrust");
-    this.cursorKeys = this.input.keyboard.createCursorKeys();
     this.player.setCollideWorldBounds(true);
 
-    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    this.projectiles = this.add.group();
+    // add bitmapTexts
+    this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE", 16);
+    this.scoreMultLabel = this.add.bitmapText(this.scale.width - 100, 5, "pixelFont", "SCORE MULT.", 16);
 
-    this.physics.add.collider(this.projectiles, this.powerUps,
-      function (projectile, powerUp) {
-        projectile.destroy();
-      });
-    this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, undefined, this);
+    // add sounds
+    this.beamSound = this.sound.add("audio_beam");
+    this.explosionSound = this.sound.add("audio_explosion");
+    this.pickupSound = this.sound.add("audio_pickup");
+    this.scoreSound = this.sound.add("leeroy");
+    this.music = this.sound.add("music");
 
-    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, undefined, this);
+    // configure music and play
+    var musicConfig = {
+      mute: false,
+      volume: 1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: false,
+      delay: 0
+    }
+    this.music.play(musicConfig);
 
-    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, undefined, this);
-
+    // make the score board at the top of the screen
+    this.score = 0;
+    this.scoreMult = 1;
+    // --makes the black rectangle background for the scoreboard--
     var graphics = this.add.graphics();
     graphics.fillStyle(0x000000, 1);
     graphics.beginPath();
@@ -132,31 +94,66 @@ export default class MainScene extends Phaser.Scene {
     graphics.closePath();
     graphics.fillPath();
 
-    this.score = 0;
-    this.scoreMult = 1;
-    this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE", 16);
-    this.scoreMultLabel = this.add.bitmapText(this.scale.width - 100, 5, "pixelFont", "SCORE MULT.", 16);
+    // add the enemies to the group and activate physics for them
+    this.enemies = this.physics.add.group();
+    this.enemies.add(this.ship1);
+    this.enemies.add(this.ship2);
+    this.enemies.add(this.ship3);
+    this.enemies.add(this.ship4);
+    this.enemies.add(this.ship5);
+    this.enemies.add(this.ship6);
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+    this.ship4.setInteractive();
+    this.ship5.setInteractive();
+    this.ship6.setInteractive();
 
-    this.beamSound = this.sound.add("audio_beam");
-    this.explosionSound = this.sound.add("audio_explosion");
-    this.pickupSound = this.sound.add("audio_pickup");
-    this.scoreSound = this.sound.add("leeroy");
+    // create a group for the projectiles
+    this.projectiles = this.add.group();
 
-    this.music = this.sound.add("music");
+    // animate the sprites
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+    this.ship4.play("ship1_anim");
+    this.ship5.play("ship2_anim");
+    this.ship6.play("ship3_anim");
+    this.player.play("thrust");
 
-    var musicConfig = {
-      mute: false,
-      volume: 1,
-      rate: 1,
-      detune: 0,
-      seek: 0,
-      loop: false,
-      delay: 0
+
+    // create the group of power ups and fill it
+    this.powerUps = this.physics.add.group();
+    var maxObjects = 4;
+    for (var i = 0; i <= maxObjects; i++) {
+      var powerUp = this.physics.add.sprite(16, 16, "power-up");
+      this.powerUps.add(powerUp);
+      powerUp.setRandomPosition(0, 0, this.scale.width, this.scale.height);
+      if (Math.random() > 0.5) {
+        powerUp.play("red");
+      } else {
+        powerUp.play("gray");
+      }
+      powerUp.setVelocity(100, 100);
+      powerUp.setCollideWorldBounds(true);
+      powerUp.setBounce(1);
     }
 
-    this.music.play(musicConfig);
+    // create keyboard interaction
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    // physics colliders and overlaps
+    this.physics.add.collider(this.projectiles, this.powerUps,
+      function (projectile, powerUp) {
+        projectile.destroy();
+      });
+    this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, undefined, this);
+    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, undefined, this);
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, undefined, this);
   }
 
+  // Pad the score with zeroes
   zeroPad(number, size) {
     var stringNumber = String(number);
     while (stringNumber.length < (size || 2)) {
@@ -165,6 +162,7 @@ export default class MainScene extends Phaser.Scene {
     return stringNumber;
   }
 
+  // interaction functions
   pickPowerUp(player, powerUp) {
     powerUp.disableBody(true, true);
     this.pickupSound.play();
@@ -231,6 +229,7 @@ export default class MainScene extends Phaser.Scene {
     this.explosionSound.play();
   }
 
+// move and reset enemy ships
   moveShip(ship, speed) {
     ship.y += speed;
 
@@ -244,34 +243,7 @@ export default class MainScene extends Phaser.Scene {
     ship.x = Phaser.Math.Between(0, this.scale.width);
   }
 
-  update() {
-    this.moveShip(this.ship1, 1);
-    this.moveShip(this.ship2, 2);
-    this.moveShip(this.ship3, 3);
-    this.moveShip(this.ship4, 1);
-    this.moveShip(this.ship5, 2);
-    this.moveShip(this.ship6, 3);
-
-    var scoreFormated = this.zeroPad(this.score, 6);
-    this.scoreLabel.text = "SCORE " + scoreFormated;
-
-    this.scoreMultLabel.text = "SCORE MULT.: " + this.scoreMult;
-
-    this.background.tilePositionY -= 0.5;
-
-    this.movePlayerManager();
-
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-      if (this.player.active) {
-        this.shootBeam();
-      }
-    }
-    for (var i = 0; i < this.projectiles.getChildren().length; i++) {
-      var beam = this.projectiles.getChildren()[i];
-      beam.update();
-    }   
-  }
-
+  // move player
   movePlayerManager() {
     if (this.cursorKeys.left.isDown) {
       this.player.setVelocityX(-200);
@@ -291,8 +263,44 @@ export default class MainScene extends Phaser.Scene {
     }
   }
 
+  // shoot beam
   shootBeam() {
     var beam = new Beam(this);
     this.beamSound.play();
+  }
+
+  // update function
+  update() {
+    // scroll background
+    this.background.tilePositionY -= 0.5;
+
+    // move enemies
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+    this.moveShip(this.ship4, 1);
+    this.moveShip(this.ship5, 2);
+    this.moveShip(this.ship6, 3);
+
+    // move player
+    this.movePlayerManager();
+
+    // update score and multiplier
+    var scoreFormated = this.zeroPad(this.score, 6);
+    this.scoreLabel.text = "SCORE " + scoreFormated;
+    this.scoreMultLabel.text = "SCORE MULT.: " + this.scoreMult;
+
+    // shoot beams when space is pressed
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      if (this.player.active) {
+        this.shootBeam();
+      }
+    }
+
+    // call the beams' update functions
+    for (var i = 0; i < this.projectiles.getChildren().length; i++) {
+      var beam = this.projectiles.getChildren()[i];
+      beam.update();
+    }   
   }
 }
