34e7f61bd610a8d3445ce8760a9bfa63339473d7
wrembish <wrembish@udel.edu>
2020-03-05 17:20:43 -0500

    Updates

---
+++ final/precache-manifest.944907b3c8bbdb272b775cb035315fc0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/font/font.xml"
+  },
+  {
+    "revision": "81bff69f26772433f24a34e8c0fefba2",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/images/background.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "80a81e3b310710596811e0a264440b28",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/leeroyJenkins.mp3"
+  },
+  {
+    "revision": "274e97411411c26e7f5cce41aa7320ec",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/leeroyJenkins.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "4fe6303092faaf432b642993b2f49c3e",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/oppa.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-wrembish/finalassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "57964f6c561eabf6146a",
+    "url": "/coding-2-try-phaser-wrembish/finalgen.main.0301ea97d990e4c1bdb2.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-wrembish/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "7196fbc7ae08ebd168e3fb9f0d1a70cb",
+    "url": "/coding-2-try-phaser-wrembish/finalindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/coding-2-try-phaser-wrembish/finalmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/coding-2-try-phaser-wrembish/finalsw.js"
+  }
+]);
